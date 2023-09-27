fd267b3a618974137b3383295d2d666a76190a48
Leonardo Savio <leosavio@leosavio.com>
2023-09-27 07:07:33 +0000

    redis ok

--- _locust/locustfile.py
+++ _locust/locustfile.py
@@ -5,13 +5,16 @@ class WebsiteUser(HttpUser):
     wait_time = between(1, 2.5)
     host = "https://8080-leosavio-testjava21gp-2r2x4edik2e.ws-us104.gitpod.io"
     counter = 0
+    keys = []
 
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
     }
 
-    names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Isaac", "Jenny"]
+    first_names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Isaac", "Jenny", "Leonardo", "Savio"]
+    last_names = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Savio", "Cesar", "Anderson", "Clark", "Davis", "Evans", "Franklin", "Harris", "Ingram", "Jackson", "King"]
+
 
     @task(1)
     def post_test(self):
@@ -29,12 +32,28 @@ class WebsiteUser(HttpUser):
 
     @task(2)
     def post_name_to_redis(self):
-        # Pick a random name
-        random_name = random.choice(WebsiteUser.names)
-        
+         # Pick a random name
+        first_name = random.choice(WebsiteUser.first_names)
+        last_name = random.choice(WebsiteUser.last_names)
+        full_name = f"{first_name} {last_name}"
+
+        # Use the counter for key's value
+        key_name = f"database:redis:{WebsiteUser.counter}"
+
         payload = {
-            "database:redis:creator": random_name
+            key_name: full_name
         }
 
-        self.client.post("/demoj21/api/redis/strings", headers=self.headers, json=payload)
-    
+        response = self.client.post("/demoj21/api/redis/strings", headers=self.headers, json=payload)
+        WebsiteUser.keys.append(key_name)
+        # Print the response content
+        print(response.content)
+
+    @task(3)
+    def get_data_from_redis(self):
+        if WebsiteUser.keys:
+            key_to_fetch = random.choice(WebsiteUser.keys)
+            response = self.client.get(f"/demoj21/api/redis/strings/{key_to_fetch}")
+            
+            # Print the response content
+            print(response.content)
